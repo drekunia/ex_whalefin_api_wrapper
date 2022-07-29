f39c6ca07b7403880ccdca91358b58f364a8142c
Andre Kurniawan <drekunia@gmail.com>
2022-07-29 22:42:09 +0700

    application.ex: supervisor tree for finch

---
+++ lib/ex_whalefin_api_wrapper/application.ex
@@ -0,0 +1,12 @@
+defmodule ExWhalefinApiWrapper.Application do
+  use Application
+  require Logger
+
+  def start(_type, _args) do
+    children = [
+      {Finch, name: FinchHTTP}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
